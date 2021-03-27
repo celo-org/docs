de35d09aff6cd07774eabcb958e11b972a30aedf
Connor McEwen <connor.mcewen@gmail.com>
2021-03-27 15:37:34 -0400

    Migrate to docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,102 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "Celo Docs",
+  tagline: "The tagline of my site",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Celo Docs",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          href: "https://celo.org",
+          label: "Celo Home",
+        },
+        {
+          href: "https://celo.org/build",
+          label: "Build",
+        },
+        {
+          href: "https://celo.org/build/faucet",
+          label: "Faucet",
+        },
+        {
+          href: "https://github.com/celo-org/docs",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Getting Started",
+              to: "docs/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+          routeBasePath: "/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
---
+++ sidebars.js
@@ -0,0 +1,26 @@
+module.exports = {
+  docs: [
+    {
+      type: "doc",
+      id: "welcome",
+    },
+    {
+      type: "doc",
+      id: "overview",
+    },
+    {
+      type: "category",
+      label: "Getting Started",
+      items: [
+        "getting-started/alfajores-testnet",
+        "getting-started/baklava-testnet",
+        "getting-started/choosing-a-network",
+        "getting-started/faucet",
+        "getting-started/hosted-nodes",
+        "getting-started/mainnet",
+        "getting-started/running-a-full-node-in-alfajores",
+      ],
+      collapsed: false,
+    },
+  ],
+};
