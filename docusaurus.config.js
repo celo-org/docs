a725f05b35f49189afe284b9b7d99f528bd2aab6
Eric Nakagawa <eric.nakagawa@clabs.co>
2021-08-16 15:10:20 -0700

    add google analytics, and sitemap generation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,102 +4,108 @@ const math = require('remark-math');
 const katex = require('rehype-katex');
 
 module.exports = {
-  title: "Celo Docs",
-  tagline: "Documentation for the Celo Platform",
-  url: "https://docs.celo.org",
-  baseUrl: "/",
-  trailingSlash: false,
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/logo.png",
-  organizationName: "celo-org", // Usually your GitHub org/user name.
-  projectName: "docs", // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: "Celo Docs",
-      logo: {
-        alt: "Celo Logo",
-        src: "img/logo.png",
-      },
-      items: [
-        {
-          href: "https://celo.org",
-          label: "Celo Home",
+    title: "Celo Docs",
+    tagline: "Documentation for the Celo Platform",
+    url: "https://docs.celo.org",
+    baseUrl: "/",
+    trailingSlash: false,
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/logo.png",
+    organizationName: "celo-org", // Usually your GitHub org/user name.
+    projectName: "docs", // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: "Celo Docs",
+            logo: {
+                alt: "Celo Logo",
+                src: "img/logo.png",
+            },
+            items: [{
+                    href: "https://celo.org",
+                    label: "Celo Home",
+                },
+                {
+                    href: "https://celo.org/build",
+                    label: "Build",
+                },
+                {
+                    href: "https://celo.org/build/faucet",
+                    label: "Faucet",
+                },
+                {
+                    href: "https://github.com/celo-org/docs",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
         },
-        {
-          href: "https://celo.org/build",
-          label: "Build",
+        footer: {
+            style: "dark",
+            links: [{
+                    title: "Docs",
+                    items: [{
+                        label: "Home",
+                        to: "/",
+                    }, ],
+                },
+                {
+                    title: "Community",
+                    items: [{
+                            label: "Stack Overflow",
+                            href: "https://stackoverflow.com/questions/tagged/celo",
+                        },
+                        {
+                            label: "Discord",
+                            href: "https://discordapp.com/invite/docusaurus",
+                        },
+                        {
+                            label: "Twitter",
+                            href: "https://twitter.com/docusaurus",
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Celo Foundatio, Inc. Built with Docusaurus.`,
         },
-        {
-          href: "https://celo.org/build/faucet",
-          label: "Faucet",
+        googleAnalytics: {
+            trackingID: 'G-W2H8BZBELD',
+            anonymizeIP: true, // Should IPs be anonymized?
         },
-        {
-          href: "https://github.com/celo-org/docs",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
     },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
+    presets: [
+        [
+            "@docusaurus/preset-classic",
             {
-              label: "Home",
-              to: "/",
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                    editUrl: "https://github.com/celo-org/docs/edit/main/",
+                    routeBasePath: "/",
+                    remarkPlugins: [math],
+                    rehypePlugins: [katex],
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
             },
-          ],
-        },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/celo",
-            },
-            {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/docusaurus",
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Celo Foundatio, Inc. Built with Docusaurus.`,
-    },
-  },
-  presets: [
-    [
-      "@docusaurus/preset-classic",
-      {
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          editUrl: "https://github.com/celo-org/docs/edit/main/",
-          routeBasePath: "/",
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      },
+        ],
     ],
-  ],
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css',
-      integrity: 'sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc',
-      crossorigin: 'anonymous',
-    }
-  ],
-  plugins: [
-    path.resolve(__dirname, 'src/plugins/aliases.ts'),
-  ]
+    stylesheets: [{
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css',
+        integrity: 'sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc',
+        crossorigin: 'anonymous',
+    }],
+    plugins: [
+        path.resolve(__dirname, 'src/plugins/aliases.ts'),
+        '@docusaurus/plugin-google-analytics',
+        [
+            '@docusaurus/plugin-sitemap',
+            {
+                changefreq: 'weekly',
+                priority: 0.5,
+                trailingSlash: false,
+            },
+        ],
+    ]
 };
