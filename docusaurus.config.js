0eafb31eadc13dab3370d71722066e8e3f694ab5
Josh Crites <critesjosh@gmail.com>
2021-09-15 16:30:36 -0400

    use regex.test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,12 +105,9 @@ module.exports = {
             {
               createRedirects: function(existingPath){
                 let regex = new RegExp('\/command-line-interface\/commands\/')
-                let match = existingPath.match(regex)
-                if (match.length === 0){
+                if (existingPath === '/command-line-interface/commands' || existingPath === '/command-line-interface/commands/'){
                     return [existingPath]
-                } else if (existingPath === '/command-line-interface/commands' || existingPath === '/command-line-interface/commands/'){
-                    return [existingPath]
-                } else if (match.length > 0) {
+                } else if (regex.test(existingPath)) {
                     return [existingPath.replace("commands/", "")]
                 }
               },
