47a9b68c634040a47669bbc821215dcfab455f5f
Joe Nyzio <jdnyzio@gmail.com>
2021-09-20 20:40:21 -0700

    Add Google Analytics
    Add tracking code for Google Analytics.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,91 +14,8 @@ module.exports = {
     favicon: "img/logo.png",
     organizationName: "celo-org", // Usually your GitHub org/user name.
     projectName: "docs", // Usually your repo name.
-    themeConfig: {
-        navbar: {
-            title: "Celo Docs",
-            logo: {
-                alt: "Celo Logo",
-                src: "img/logo.png",
-            },
-            items: [{
-                    href: "https://celo.org",
-                    label: "Celo Home",
-                },
-                {
-                    href: "https://celo.org/build",
-                    label: "Build",
-                },
-                {
-                    href: "https://celo.org/build/faucet",
-                    label: "Faucet",
-                },
-                {
-                    href: "https://github.com/celo-org/docs",
-                    label: "GitHub",
-                    position: "right",
-                },
-            ],
-        },
-        algolia: {
-            apiKey: 'a55b84f8b98dc5edd71d11cf4e42434e',
-            indexName: 'celo',
-            contextualSearch: false,
-            debug: false
-        },
-        footer: {
-            style: "dark",
-            links: [{
-                    title: "Docs",
-                    items: [{
-                        label: "Home",
-                        to: "/",
-                    }, ],
-                },
-                {
-                    title: "Community",
-                    items: [{
-                            label: "Stack Overflow",
-                            href: "https://stackoverflow.com/questions/tagged/celo",
-                        },
-                        {
-                            label: "Discord",
-                            href: "https://discordapp.com/invite/docusaurus",
-                        },
-                        {
-                            label: "Twitter",
-                            href: "https://twitter.com/docusaurus",
-                        },
-                    ],
-                },
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} Celo Foundation, Inc. Built with Docusaurus.`,
-        },
-    },
-    presets: [
-        [
-            "@docusaurus/preset-classic",
-            {
-                docs: {
-                    sidebarPath: require.resolve("./sidebars.js"),
-                    // Please change this to your repo.
-                    editUrl: "https://github.com/celo-org/docs/edit/main/",
-                    routeBasePath: "/",
-                    remarkPlugins: [math],
-                    rehypePlugins: [katex],
-                },
-                theme: {
-                    customCss: require.resolve("./src/css/custom.css"),
-                },
-            },
-        ],
-    ],
-    stylesheets: [{
-        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css',
-        integrity: 'sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc',
-        crossorigin: 'anonymous',
-    }],
     plugins: [
+        '@docusaurus/plugin-google-gtag',
         path.resolve(__dirname, 'src/plugins/aliases.ts'),
         [
             '@docusaurus/plugin-client-redirects',
@@ -370,5 +287,95 @@ module.exports = {
               ],
             },
         ]
-    ]
+    ],
+    themeConfig: {
+        navbar: {
+            title: "Celo Docs",
+            logo: {
+                alt: "Celo Logo",
+                src: "img/logo.png",
+            },
+            items: [{
+                    href: "https://celo.org",
+                    label: "Celo Home",
+                },
+                {
+                    href: "https://celo.org/build",
+                    label: "Build",
+                },
+                {
+                    href: "https://celo.org/build/faucet",
+                    label: "Faucet",
+                },
+                {
+                    href: "https://github.com/celo-org/docs",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
+        },
+        gtag: {
+            // You can also use your "G-" Measurement ID here.
+            trackingID: 'G-0CXEKQ81V2',
+            // Optional fields.
+            anonymizeIP: true, // Should IPs be anonymized?
+        },
+        algolia: {
+            apiKey: 'a55b84f8b98dc5edd71d11cf4e42434e',
+            indexName: 'celo',
+            contextualSearch: false,
+            debug: false
+        },
+        footer: {
+            style: "dark",
+            links: [{
+                    title: "Docs",
+                    items: [{
+                        label: "Home",
+                        to: "/",
+                    }, ],
+                },
+                {
+                    title: "Community",
+                    items: [{
+                            label: "Stack Overflow",
+                            href: "https://stackoverflow.com/questions/tagged/celo",
+                        },
+                        {
+                            label: "Discord",
+                            href: "https://discordapp.com/invite/docusaurus",
+                        },
+                        {
+                            label: "Twitter",
+                            href: "https://twitter.com/docusaurus",
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Celo Foundation, Inc. Built with Docusaurus.`,
+        },
+    },
+    presets: [
+        [
+            "@docusaurus/preset-classic",
+            {
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                    editUrl: "https://github.com/celo-org/docs/edit/main/",
+                    routeBasePath: "/",
+                    remarkPlugins: [math],
+                    rehypePlugins: [katex],
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            },
+        ],
+    ],
+    stylesheets: [{
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css',
+        integrity: 'sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc',
+        crossorigin: 'anonymous',
+    }],
 };
