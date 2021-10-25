bf73b2b84100b802cc09529e038f1f8940cb8d1b
Josh <critesjosh@gmail.com>
2021-10-25 13:31:26 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -349,7 +349,7 @@ module.exports = {
                     },
                     {
                         href: "https://celo.crowdin.com/celo-docs",
-                        label: "Translate"
+                        label: "Help translate"
                     },
                 ]},
                 {
