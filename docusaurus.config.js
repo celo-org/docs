583546b1a62d05e5b699d2cefb0b5869ceb455fe
Josh Crites <critesjosh@gmail.com>
2021-11-21 12:13:23 -0500

    show blog page est reading time

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -484,6 +484,12 @@ module.exports = {
                 theme: {
                     customCss: require.resolve("./src/css/custom.css"),
                 },
+                blog: {
+                    showReadingTime: true,
+                    readingTime: ({content, frontMatter, defaultReadingTime}) =>
+                        // allows per post reading time override in frontmatter
+                        frontMatter.hide_reading_time ? undefined : defaultReadingTime({content, options: {wordsPerMinute: 300}}),
+                }
             },
         ],
     ],
