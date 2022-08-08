0b7b2e3d7e0d7afbcc0ba3efde5ad7e1edaadf18
Martín Volpe <volpe@clabs.co>
2022-08-08 16:44:47 +0200

    Running an oracle (#394)

--- sidebars.js
+++ sidebars.js
@@ -925,6 +925,11 @@ const sidebars = {
         },
         {
           type: "doc",
+          label: "Running an oracle",
+          id: "getting-started/running-oracles",
+        },
+        {
+          type: "doc",
           id: "celo-codebase/protocol/oracles/band-protocol-how-to",
         },
         {
