ba3700551dd830692ab7870d038f8f7fbf8aa972
Joe Nyzio <jdnyzio@gmail.com>
2022-10-20 21:27:49 -0400

    move metamask tutorial to docs
    original https://developers.celo.org/3-simple-steps-to-connect-your-metamask-wallet-to-celo-732d4a139587

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -820,7 +820,7 @@ const Users: User[] = [
     title: '3 Simple Steps to Connect your MetaMask Wallet To Celo',
     description: 'A step-by-step tutorial to add the Celo network to your MetaMask wallet.',
     preview: require('./showcase/3-simple-steps-to-connect-your-metamask-wallet-to-celo.png'),
-    website: 'https://medium.com/celodevelopers/3-simple-steps-to-connect-your-metamask-wallet-to-celo-732d4a139587',
+    website: 'blog/tutorials/3-simple-steps-to-connect-your-metamask-wallet-to-celo',
     tags: ['basics','foundation', 'metamask'],
   },
   {
