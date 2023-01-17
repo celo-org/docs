f274ca63e4e928a87fa1fe6740b32187a1efa0f5
Phenzic <69092079+Julius170@users.noreply.github.com>
2023-01-17 17:57:46 +0100

    Sage/phenzic - Unit Testing for smart contract with Truffle and Celo (#551)
    * Create How-to-write-unit-testing-for-smart-contracts-with-truffle.md
    
    * Create A Deep Dive into Celo's Proof-of-Stake Consensus.md
    
    * Delete How-to-write-unit-testing-for-smart-contracts-with-truffle.md
    
    * Create how_to_write_unit_testing_for_smart_contract_with_truffle.md
    
    * Update how_to_write_unit_testing_for_smart_contract_with_truffle.md
    
    * Update how_to_write_unit_testing_for_smart_contract_with_truffle.md
    
    * Update users.tsx
    
    * Create A_Deep_Dive_into_Celo's_Proof_of_Stake_Consensus.md
    
    * Update users.tsx
    
    * feat: formatting fix
    
    Co-authored-by: Viral Sangani <viral.sangani2011@gmail.com>

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -1413,7 +1413,21 @@ const Users: User[] = [
     description: 'How to write unit testing for smart contracts with Hardhat.',
     preview: require('./showcase/intermediate/how-to-write-unit-testing-for-smart-contracts-with-hardhat.png'),
     website: 'blog/tutorials/how-to-write-unit-testing-for-contracts-with-hardhat',
-    tags: ['intermediate', 'hardhat'],
+    tags: ['intermediate', 'hardhat', 'celosage', 'smartcontract'],
+  },
+  {
+    title: 'Unit Testing with Solidity and Truffle',
+    description: 'How to write unit testing for smart contracts with Truffle.',
+    preview: require('./showcase/beginner/how-to-write-unit-testing-for-contracts-with-truffle.png'),
+    website: 'blog/tutorials/how-to-write-unit-testing-for-contracts-with-truffle',
+    tags: ['intermediate', 'truffle', 'smartcontract', 'celosage'],
+  },
+  {
+    title: 'Proof Of Stack Consensus',
+    description: 'A Deep Dive into Celo Proof of Stake Consensus.',
+    preview: require('./showcase/beginner/a-deep-dive-into-celo-proof-of-stake-consensus.png'),
+    website: 'blog/tutorials/a-deep-dive-into-celo-proof-of-stake-consensus',
+    tags: ['intermediate', 'smartcontract', 'celosage', 'celo'],
   },
   {
     title: 'Minting your ERC1155 Token on Celo',
