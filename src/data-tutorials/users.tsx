8109ced38d4f1852781908a267a97529634213d6
boyfem <47746690+boyfem@users.noreply.github.com>
2023-02-18 12:49:08 +0100

    [sage] added new article

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -695,8 +695,15 @@ const Users: User[] = [
     title: 'How to Use Hardware Tools with the Celo Blockchain',
     description: 'In this series, we will explore how hardware is driving the adoption of the Celo blockchain',
     preview: require('./showcase/celo-sage.png'),
-    website: 'blog/tutorials/how-to-use-hardware-tools-with-the-celo-blockchain',
+    website: '/blog/tutorials/how-to-use-hardware-tools-with-the-celo-blockchain',
     tags: ['backend','intermediate','technical'],
+  },
+  {
+    title: 'Why Should You Build Your Next Project on the Celo Blockchain?',
+    description: 'This tutorial examines the features and capabilities of the Celo Blockchain',
+    preview: require('./showcase/celo-sage.png'),
+    website: '/blog/tutorials/why-you-should-build-your-next-project-on-the-celo-blockchain',
+    tags: ['duplicate','beginner','theoretical'],
   },
   {
     title: ' Celo Composer',
