495d28f1499a26cc5f09f91508b9f4b0a58f9f59
guizostudios <97633823+guizostudios@users.noreply.github.com>
2023-03-02 11:59:27 -0500

    Ewerton - Celo Sage - How to tokenize physical assets on Celo (#664)
    * done
    
    * Updated
    
    * added solidity to the code markdown
    
    * fix: formatting and images
    
    ---------
    
    Co-authored-by: Viral Sangani <viral.sangani2011@gmail.com>

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -704,6 +704,13 @@ const Users: User[] = [
     tags: ['celosage', 'beginner','celo'],
   },
   {
+    title: 'How to Tokenize Physical Assets on Celo',
+    description: 'Understand how to start the tokenization on Celo',
+    preview: require('./showcase/beginner/how-to-tokenize.png'),
+    website: 'blog/tutorials/how-to-tokenize-a-physical-asset-on-celo',
+    tags: ['celosage', 'beginner','celo', 'smartcontract'],
+  },
+  {
     title: '1. Beginner Tutorials',
     description: 'Curated list of beginner Celo developer tutorials.',
     preview: require('./showcase/beginner/beginner-tutorials.png'),
