a0ec06c9378555fd01552340bbf3be3ef9a53467
Oyeniyi Abiola Peace <38940247+iamoracle@users.noreply.github.com>
2023-03-02 17:14:32 +0100

    Sage/cryptography in blockchain an overview of hash functions and digital signatures (#676)
    * added a new article
    
    * resolved conflicts
    
    * solved title error
    
    * added header image
    
    * added author bio
    
    * reformatted headers
    
    * fix: issues
    
    ---------
    
    Co-authored-by: Viral Sangani <viral.sangani2011@gmail.com>

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -779,6 +779,13 @@ const Users: User[] = [
     tags: ['celosage', 'android', 'celo', 'dapp', 'mobile', 'sdk', 'valora', 'advanced'],
   },
   {
+    title: 'Cryptography in Blockchain - An Overview of Hash Functions and Digital Signatures',
+    description: ' Cryptography plays a critical role in ensuring the security and privacy of data in the blockchain, the Distributed Ledger Technology (DLT) that has gained widespread adoption in recent years.',
+    preview: require('./showcase/intermediate/cryptography-in-blockchain-an-overview-of-hash-functions-and-digital-signatures.png'),
+    website: '/blog/tutorials/cryptography-in-blockchain-an-overview-of-hash-functions-and-digital-signatures',
+    tags: ['celosage', 'intermediate'],
+  },
+  {
     title: 'Build In Public - Tips for making an impact',
     description: 'In this article we will discuss the benifits of having an online presence and the ways it can make your developer experience easy and enjoyable.',
     preview: require('./showcase/beginner/build-in-public.png'),
@@ -867,6 +874,13 @@ const Users: User[] = [
     preview: require('./showcase/advanced/c-sharp-mobile-dapp-to-display-celo-nfts.png'),
     website: 'https://docs.celo.org/blog/2022/07/15/csharp-mobile-app-to-display-celo-nfts',
     tags: ['advanced','foundation', 'mobile', 'android', 'nft', 'popular'],
+  },
+  {
+    title: 'Flutter & Celo - Easily build Flutter Mobile dApps',
+    description: 'Celo Composer now supports Flutter. Quickly develop mobile apps on Celo.',
+    preview: require('./showcase/intermediate/flutter-and-celo-easily-build-flutter-mobile-dapps.png'),
+    website: 'blog/tutorials/flutter-celo-easily-build-flutter-mobile-dApps',
+    tags: ['intermediate','foundation', 'flutter', 'composer'],
   },
   {
     title: "Flutter & Celo - Easily build Flutter Mobile dApps",
