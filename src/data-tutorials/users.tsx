7d2039fd336d5abd6c2ae2edb566e0dbb8b5d702
Mudasiru Rasheed <rasheedrtm1@gmail.com>
2023-03-18 20:59:59 +0100

    Update src/data-tutorials/users.tsx
    Co-authored-by: KunalOfficial <35455566+developerkunal@users.noreply.github.com>

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -26,7 +26,6 @@ import { sortBy } from "@site/src/utils/jsUtils";
  * - Use relevant tags to categorize your site (read the tag descriptions below)
  * - Add a local image preview (decent screenshot of your Docusaurus site)
  * - The image MUST be added to the  repository, and use `require("img")`
- repository, and use `require("img")`
  * - The image has to have minimum width 640 and an aspect of no wider than 2:1
  * - If your website is open-source, add your source link. The link should open
  *   to a directory containing the `docusaurus.config.js` file
