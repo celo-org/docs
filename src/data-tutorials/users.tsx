8c83f96e5cfc89e6d67eae9deb0613ad0a3063f8
developerkunal <dawar.kunal703@gmail.com>
2023-03-20 01:11:35 +0530

    Fixed PR

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -1155,7 +1155,7 @@ const Users: User[] = [
     title: 'Building A Restaurant Coupon NFT System on Celo',
     description: 'We would be going through building a restaurant coupon NFT system where restaurants can add their coupons and others can buy it',
     preview: require('./showcase/advanced/building-a-restaurant-coupon-nft-system.png'),
-    website: '/blog/tutorials/building-a-restaurant-nft-coupon-system',
+    website: '/blog/tutorials/building-a-restaurant-coupon-nft-system',
     tags: ['celo', 'advanced', 'celosage', 'nft', 'hardhat'],
   },
   {
