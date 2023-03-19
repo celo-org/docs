89f68b3b7d242d0356e149638a3b939c05b2e9ee
danielogbuti <danielogbuti@gmail.com>
2023-03-19 20:21:55 +0100

    users.tsx

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -1152,11 +1152,11 @@ const Users: User[] = [
     tags: ['celo', 'intermediate', 'celosage'],
   },
   {
-    title: 'Celo Integration with Web3',
-    description: ' A Look at How the Platform is Bridging the Gap Between Centralized and Decentralized Systems',
-    preview: require('./showcase/advanced/web3.png'),
-    website: '/blog/tutorials/celo-integration-with-web3',
-    tags: ['celo', 'advanced', 'solidity', 'celosage'],
+    title: 'Building A Restaurant Coupon NFT System on Celo',
+    description: 'We would be going through building a restaurant coupon NFT system where restaurants can add their coupons and others can buy it',
+    preview: require('./showcase/advanced/building-a-restaurant-coupon-nft-system.png'),
+    website: '/blog/tutorials/building-a-restaurant-nft-coupon-system',
+    tags: ['celo', 'advanced', 'celosage', 'nft', 'hardhat'],
   },
   {
     title: 'Building for the Celo Connect Mobile Hackathon',
