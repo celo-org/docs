b34875acb0a8f3a4b8f5f07275d8b889d845aed6
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-03-20 21:31:07 +0400

    [Oluwafemi Alofe]-Build an Airdrop Distribution System for Millions of Users with Verification of Merkle Tree Proofs (#798)
    * [Oluwafemi Alofe]-Build an Airdrop Distribution System for Millions of Users with Verification of Merkle Tree Proofs
    
    * [Oluwafemi Alofe]-Build an Airdrop Distribution System for Millions of Users with Verification of Merkle Tree Proofs
    
    * Add author about
    
    * Update build-an-airdrop-distribution-system-for-millions-of-users-with-verification-of-merkle-tree-proofs.md
    
    * Update build-an-airdrop-distribution-system-for-millions-of-users-with-verification-of-merkle-tree-proofs.md
    
    * Update build-an-airdrop-distribution-system-for-millions-of-users-with-verification-of-merkle-tree-proofs.md
    
    * Update build-an-airdrop-distribution-system-for-millions-of-users-with-verification-of-merkle-tree-proofs.md
    
    * Update build-an-airdrop-distribution-system-for-millions-of-users-with-verification-of-merkle-tree-proofs.md
    
    * Fixed Markdown
    
    ---------
    
    Co-authored-by: KunalOfficial <35455566+developerkunal@users.noreply.github.com>
    Co-authored-by: developerkunal <dawar.kunal703@gmail.com>

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -2393,6 +2393,13 @@ const Users: User[] = [
     website: "/blog/tutorials/build-a-monthly-susbscription-platform-using-celo-composer-openzeppelin-defender",
     tags: ["advanced", "celosage", "celo", "openzeppelin"],
   },
+  {
+    title: "Build an Airdrop Distribution System for Millions of Users with Verification of Merkle Tree Proofs",
+    description: "Curious about how Uniswap and other projects are able to airdrop tokens to thousands of users? In this tutorial, we will show you how they use Merkle proof in Solidity and Javascript to accomplish this feat.",
+    preview: require("./showcase/advanced/build-an-airdrop-distribution-system-for-millions-of-users-with-verification-of-merkle-tree-proofs.png"),
+    website: "/blog/tutorials/build-an-airdrop-distribution-system-for-millions-of-users-with-verification-of-merkle-tree-proofs.md",
+    tags: ["advanced", "celosage", "celo", "tokens"],
+  },
   /*
   Pro Tip: add your site in alphabetical order.
   Appending your site here (at the end) is more likely to produce Git conflicts.
