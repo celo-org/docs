e903ff4595b9490e67c36ff8b437bf645785e7c3
Muhideen Mujeeb Adeoye <41507005+Mujhtech@users.noreply.github.com>
2023-03-20 13:18:07 +0100

    [Muhideen Mujeeb] - Sage - How to build a Celo Price Tracker browser extension using Vite and Celo Contractkit. (#767)
    * added content files
    
    * fixed
    
    * Markdown fix
    
    * added github link
    
    * Fixed delete unnessary file
    
    ---------
    
    Co-authored-by: developerkunal <dawar.kunal703@gmail.com>

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -2266,6 +2266,13 @@ const Users: User[] = [
     website: 'blog/tutorials/interact-with-celo-blockchain-using-web3dart',
     tags: ['celosage', 'flutter', 'beginner', 'celo'], 
   },
+  {
+    title: 'How to build a Celo Price Tracker browser extension using Vite and Celo Contractkit.',
+    description: 'In this article, I will show developers how to create a Celo Price Tracker browser extension that works with any browser, such as Brave, Chrome, and Firefox, by using Vite (a React template), Crxjs Vite Plugin, and the Celo Contractkit package.',
+    preview: require('./showcase/beginner/how-to-build-a-celo-price-tracker-browser-extension-using-vite-and-celo-contractkit.png'),
+    website: 'blog/tutorials/how-to-build-a-celo-price-tracker-browser-extension-using-vite-and-celo-contractkit',
+    tags: ['celosage', 'react', 'beginner', 'celo'],
+    },
    {
     title: 'Build a Full-Stack Mobile DApp with React Native and Web.JS on Celo',
     description: 'Learn hoe to build chat app on celo network using react-native and web3js',
