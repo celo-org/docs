6021bbf96c7c92985d23b8649651f8c0b7c70015
John Fáwọlé <johnfawole18@gmail.com>
2023-04-14 20:23:57 +0100

    Sage]   factory contracts on celo (#710)
    * Create Factory-contract-on-celo.md
    
    * Update users.tsx
    
    * Add files via upload
    
    * Delete factory cover.png
    
    * Add files via upload
    
    * Update Factory-contract-on-celo.md
    
    * Fixed spacing issues.
    
    * Update Factory-contract-on-celo.md
    
    * Update Factory-contract-on-celo.md
    
    * Add files via upload
    
    * Update Factory-contract-on-celo.md
    
    * Update Factory-contract-on-celo.md
    
    * fixed:header-image-src-link,fixed:formatting
    
    * added;tags
    
    * Fixed Title
    
    ---------
    
    Co-authored-by: Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
    Co-authored-by: developerkunal <dawar.kunal703@gmail.com>

--- src/data-tutorials/users.tsx
+++ src/data-tutorials/users.tsx
@@ -1216,6 +1216,13 @@ const Users: User[] = [
     website: 'blog/tutorials/como-implantar-um-contrato-inteligente-ERC721-usando-a-API-Tatum',
     tags: ['intermediate','celosage','erc721','smartcontract','video'],
   },
+ {
+  title: 'How to Build and Deploy Factory Contracts on Celo Blockchain',
+  description: 'This article will demonstrate how to use the factory pattern to correctly deploy multiple instances of your smart contract',
+  preview: require('./showcase/intermediate/factorycover.png'),
+  website: 'blog/tutorials/how-to-build-and-deploy-factory-contracts-on-celo',
+  tags: ['solidity', 'intermediate', 'celosage', 'smartcontract', 'celo'],
+  },
   {
     title: 'Aprenda sobre Oraculos em Celo',
     description: 'Aprenda e entenda mais sobre oraculos em Celo.',
