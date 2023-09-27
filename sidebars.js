6fbb5b56cb5060f2ad2331e019b5f701fa7e8c17
Karl Bartel <karl.bartel@clabs.co>
2023-09-27 16:57:06 +0200

    Revert "Document CIP-20/30/31 precompiles (#872)" (#1130)
    This reverts commit 6b9db650c81e23fb42ed14b7cc25ef03b6ede178.
    
    The precompiles have been removed in the Gingerbread hardfork.

--- sidebars.js
+++ sidebars.js
@@ -896,11 +896,6 @@ const sidebars = {
         },
         {
           type: "doc",
-          label: "Crypto Precompiles",
-          id: "protocol/precompiles",
-        },
-        {
-          type: "doc",
           label: "Ultralight Client",
           id: "protocol/plumo",
         },
