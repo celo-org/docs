6f5e22ae8597e87702b970d0c3f9e28d4e063ccd
arthurgousset <46296830+arthurgousset@users.noreply.github.com>
2024-05-15 14:01:05 +0200

    fix(scripts/copy-generated-docs): adds `git pull`
    Previously, the script didn't fetch the latest commit from master.
    With this change, I'm hoping it will always fetch the latest commit.

--- scripts/copy-generated-docs.sh
+++ scripts/copy-generated-docs.sh
@@ -1,4 +1,5 @@
 git submodule init
 git submodule update
+git submodule foreach git pull origin master
 cp -R ./submodules/developer-tooling/docs/command-line-interface/* ./docs/cli/
 yarn write-heading-ids docs/cli/*
