03e1930d8ffd788ecb80b76d88989ce90d681795
Victoria <4222953+lvpeschke@users.noreply.github.com>
2025-03-11 14:01:35 +0100

    Add redirect for alternative link to community RPC node docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,6 +39,10 @@ module.exports = {
             from: "/cel2/l2-operator-guide",
             to: "/cel2/operators/overview",
           },
+          {
+            from: "/cel2/operators/rpc-provider",
+            to: "/cel2/operators/community-rpc-node",
+          },
         ],
       },
     ],
