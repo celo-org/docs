6a2d0185e0629baeb2947c23e6fe85fdcc719f1b
Javier Cortejoso <javier.cortejoso@gmail.com>
2026-04-27 18:28:38 +0200

    Point lemonade links to lemonade.social/s/celogovernance
    Replace all lemonade.social/s/celo references with the correct
    governance events URL lemonade.social/s/celogovernance. Rename
    labels from "Events" to "Governance Events".

--- snippets/home.jsx
+++ snippets/home.jsx
@@ -476,7 +476,7 @@ export const CeloBuilderEcosystem = ({
     {
       title: "Bring Your Ideas to Life",
       desc: "Sign up for upcoming hackathons and workshops",
-      href: "https://lemonade.social/s/celo",
+      href: "https://lemonade.social/s/celogovernance",
       external: true,
       icon: "img/homepage/contribute.svg",
     },
